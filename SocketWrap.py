0dd2c630b8f01c64612d7f04cd0a82d1dc782e38
Peter Valtersson <peter.walter.valtersson@gmail.com>
2018-08-31 13:12:20 +0200

    More cleaning

--- SocketWrap.py
+++ SocketWrap.py
@@ -17,7 +17,11 @@ LIST = 8
 class InvalidAddressOrPort(Exception)
 	def __init__(self, address, port):
 		super().__init__("Invalid address or port. Address: {}, Port: {}".format(address, port))
-
+class ConnectionLost(Exception)
+	def __init__(self, socket):
+		super().__init__("Connection to {}:{} lost".format(socket.address, socket.port) 
+		self.socket = socket
+		
 ## A wrapper for sockets that automatically resolves what is going to be sent and received.
 #
 # This class simplifies the send and recv by automatically resolving the type of what is being sent/received.
@@ -27,64 +31,62 @@ class Socket():
 	## Constructor
 	#
 	# @param socket The socket to wrap. If None will create a new socket
-	# @param cleanupCallback Function to call if clean function is called.
-	def __init__(self, socket = None, cleanupCallback = lambda:None):
+	def __init__(self, socket = None):
 		if socket is None:
 			socket = socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket = socket
-		self.cleanupCallback = cleanupCallback
+		self.address = None
+		self.port = None
 		try:
 			import bluetooth as bt
 			if type(self.socket) is bt.BluetoothSocket:
-				self.recv_ = self._bluetoothRecv
+				self._recv = self._bluetoothRecv
 		except:
 			# No bluetooth library
 			pass	
-	## Overrides recv_ when using bluetooth
+	## Overrides _recv when using bluetooth
 	#
 	# Since bluetoothSocket does not have recv_into, this is used instead.
-	# \see recv_
+	# \see _recv
 	def _bluetoothRecv(self, size):
 		ret = bytearray(size)
 		view = memoryview(ret)
 		toRead = size
 		while toRead:
 			bytes = self.socket.recv(min(toRead, LONG_STR_LENGHT))
-			if bytes == b'': raise LHMT_ConnectionLost(self)	
+			if bytes == b'': raise ConnectionLost(self)	
 			nbytes = len(bytes)
 			view[:nbytes] = bytes
 			view = view[nbytes:]
 			toRead -= nbytes
 		return ret
 	
-	## Override for base socket
-	def connect(self,*args):
-		self.socket.connect(*args)
+	## Bride to base socket
+	def connect(self, address, port):
+		self.address = address
+		self.port = port
+		self.socket.connect((address, port))
 		
-	## Override for base socket
+	## Bride to base socket
 	def close(self):
 		self.socket.close()
 	
-	## Override for base socket
+	## Bride to base socket
 	def settimeout(self, *args):
 		self.socket.settimeout(*args)
-		
-	## Override for base socket
-	def Delete(self):
-		self.listener()
 		
 	## Send a message. Will send size first followed by the message.
 	#
 	# Will first send a 32bit integer representing the size, followed immediatly by the message
 	# \warning This is intended for internal purposes and should not be used from the outside
-	# \exception LHMT_ConnectionLost Connection to socket lost.
-	def send(self, msg):
+	# \exception ConnectionLost Connection to socket lost.
+	def _send(self, msg):
 		size = len(msg)
 		sent = self.socket.sendall(size.to_bytes(4, 'big'))
-		if sent == 0: raise LHMT_ConnectionLost(self)
+		if sent == 0: raise ConnectionLost(self)
 	
 		sent = self.socket.sendall(msg)
-		if sent == 0: raise LHMT_ConnectionLost(self)
+		if sent == 0: raise ConnectionLost(self)
 		return sent
 	
 	## Receive a message of the given size
@@ -93,13 +95,13 @@ class Socket():
 	# \warning This is intended for internal purposes and should not be used from the outside
 	# Creates a bytearray and a view to that array, then fills the array using recv_into until the exact number of bytes has been received.
 	# \warning Should 
-	def recv_(self, size):
+	def _recv(self, size):
 		bytes = bytearray(size)
 		view = memoryview(bytes)
 		toRead = size
 		while toRead:
 			nbytes = self.socket.recv_into(view, min(toRead, LONG_STR_LENGHT))
-			if nbytes == 0: raise LHMT_ConnectionLost(self)	
+			if nbytes == 0: raise ConnectionLost(self)	
 			view = view[nbytes:]
 			toRead -= nbytes
 		return bytes
@@ -110,9 +112,9 @@ class Socket():
 	# \warning Must be matched with a send
 	# This method will first wait for the size of the message. This is in the form of a 32bit integer.
 	# The message is then received.
-	def recv(self):
-		size = int.from_bytes(self.recv_(4), 'big')
-		return self.recv_(size)
+	def _recvData(self):
+		size = int.from_bytes(self._recv(4), 'big')
+		return self._recv(size)
 		
 	
 	## Send the type of the object to the connection
@@ -122,58 +124,53 @@ class Socket():
 	# \warning This is intended for internal purposes and should not be used from the outside
 	# @param type The type of the object
 	# @param data Additional data to send, can be the actual value of for example an int or the size of an array. This speeds up communication for small types, by removing the need to send multiple messages.
-	def SendType(self, type, data = ()):
-		try:
-			toSend = json.dumps((type, data)).encode()
-		except:
-			toSend = pickle.dumps((type, data))
-	
-		self.send(toSend) 
+	def __sendType(self, type, data = ()):
+		try: toSend = json.dumps((type, data)).encode()
+		except: toSend = pickle.dumps((type, data))
+		self._send(toSend) 
 		
 	## Recieve the type of a message
 	#
 	# This is the first thing that is called when receiving anything from the connection
 	# It will first type to using json.loads, if this fails it will revert to pickle.loads
 	# \warning This is intended for internal purposes and should not be used from the outside
-	def RecvType(self):
-		recv = self.recv()
-		try:
-			return json.loads(recv.decode()) 		
-		except:
-			return pickle.loads(recv)
+	def _recvType(self):
+		data = self._recvData()
+		try: return json.loads(data.decode()) 		
+		except: return pickle.loads(data)
 			
 	## Send a string
 	#
-	# If the string is less than LONG_STR_LENGHT-100 the string is sent in the payload other wise the string is sent separately. This avoids dumping a long string, which can reduce performance.
+	# If the string is less than LONG_STR_LENGHT-100 the string is sent in the payload otherwise the string is sent separately. This avoids dumping a long string, which can reduce performance.
 	# \warning This is intended for internal purposes and should not be used from the outside
-	def SendString(self, msg):	
+	def _sendString(self, msg):	
 		with Status_Debug("Sending string"):
 			toSend = len(msg)
 			printDebug("Length:", toSend)
 			if toSend < LONG_STR_LENGHT:
-				with Status_Debug("Sending short string"):	self.SendType(SHORT_STR, msg)
+				with Status_Debug("Sending short string"):	self._sendType(SHORT_STR, msg)
 			else:
 				with Status_Debug("Sending long string"):
-					with Status_Debug("Sendning type"): self.SendType(LONG_STR)		
-					with Status_Debug("Sending string..."):  self.send(msg.encode())
+					with Status_Debug("Sendning type"): self._sendType(LONG_STR)		
+					with Status_Debug("Sending string..."):  self._send(msg.encode())
 					
 	## Send a numpy array
 	#
 	# First sends the type, shape and dtype
 	# Then sends the array itself
 	# \warning This is intended for internal purposes and should not be used from the outside	
-	def SendNumpyArray(self, array):
-		self.SendType(NUMPY_ARRAY, (array.shape, array.dtype))
-		self.send(array.tobytes())
+	def _sendNumpyArray(self, array):
+		self._sendType(NUMPY_ARRAY, (array.shape, array.dtype))
+		self._send(array.tobytes())
 	
 	## Recieve a numpy array
 	#
 	# \warning This is intended for internal purposes and should not be used from the outside	
 	# This function constructs a numpy array and files the array with data from the connection
 	# @param shape_dtype Tuple containing the shape and dtype (in that order).
-	def RecvNumpyArray(self, shape_dtype ):
+	def _recvNumpyArray(self, shape_dtype ):
 		shape, dtype = shape_dtype
-		bytes = self.recv()
+		bytes = self._recv()
 		array = np.frombuffer(bytes, dtype)
 		array.shape = shape
 		return array
@@ -184,24 +181,24 @@ class Socket():
 	# Then sends the file itself. 
 	# \warning This is intended for internal purposes and should not be used from the outside	
 	# @param file The file to send (assumed to be opened)
-	# \warning Will not work with bluetooth, since pyBluez does not have sendfile
-	def SendFile(self, file):
+	# \warning Will not work with bluetooth, since pyBluez does not have sendfile. TODO: Implement replacement
+	def _sendFile(self, file):
 		with Status_Info("Sending file"):
 			with Status_Debug("Sending type and size"):
 				file.seek(0, 2)
 				size = file.tell()
-				self.SendType(FILE, (size, file.name)) 
+				self._sendType(FILE, (size, file.name)) 
 				file.seek(0)			
 			with Status_Debug("Sending the file"):
 				sent = self.socket.sendfile(file, 0)	
-				if sent == 0: raise LHMT_ConnectionLost(self)
+				if sent == 0: raise ConnectionLost(self)
 	
 	## Recieve a file
 	#
 	# \warning This is intended for internal purposes and should not be used from the outside	
 	# @param size_name A tuple with the size and name (in that order) of the file
 	# @param filename If None(default) the file will be created with the same name in the current working directory.
-	def RecvFile(self, size_name, filename = None):
+	def _recvFile(self, size_name, filename = None):
 		with Status_Info("Recieving file"):
 			print(size_name)
 			with Status_Debug("Recieving the file"):
@@ -210,7 +207,7 @@ class Socket():
 					toRead = size_name[0]
 					while toRead:
 						bytes = self.socket.recv(min(toRead, LONG_STR_LENGHT))
-						if bytes == b'': raise LHMT_ConnectionLost(self)	
+						if bytes == b'': raise ConnectionLost(self)	
 						file.write(bytes)
 						toRead -= len(bytes)
 		return filename
@@ -218,26 +215,26 @@ class Socket():
 	
 	## This function will automatically resolve the type of the message and communicate this with the connection and send the message.
 	#
-	# All types that can be pickled using pickle are supported, however, pickle is slow, therefore a warning is printed each time something is sent using pickle.
+	# All types that can be pickled using pickle are supported, however, pickle can be slow, therefore a warning is printed each time something is sent using pickle.
 	# \warning When sending files, they are assumed to already be opened using f = open(...)
 	def Send(self, msg):
 		if type(msg) == list or type(msg) == tuple:
-			self.SendType(LIST, msg)
+			self._sendType(LIST, msg)
 		elif isinstance(msg, str):
-			self.SendString(msg)
+			self._sendString(msg)
 		elif isinstance(msg, int):
-			self.SendType(INT, msg)
+			self._sendType(INT, msg)
 		elif isinstance(msg, Enum):
-			self.SendType(ENUM, msg.value)
+			self._sendType(ENUM, msg.value)
 		elif type(msg) is np.ndarray:
-			self.SendNumpyArray(msg)
+			self.__sendNumpyArray(msg)
 		elif type(msg).__name__ == 'BufferedReader':
-			self.SendFile(msg)	
+			self.__sendFile(msg)	
 		else:
-			printWarning("Sending with pickle") # This is slow!
-			self.SendType(PICKLE)	
+			printWarning("Sending with pickle") # This can be slow!
+			self._sendType(PICKLE)	
 			bytes = pickle.dumps(msg, pickle.HIGHEST_PROTOCOL)
-			self.send(bytes)
+			self._send(bytes)
 	
 	## Recieve a message from the connection
 	#
@@ -245,23 +242,23 @@ class Socket():
 	# @param *args Any arguments to pass to the internal methods
 	# @param **kwargs Any keyword arguments to pass to the internal methods
 	def Recv(self, *args, **kwargs):
-		type, msg = self.RecvType()
+		type, msg = self._recvType()
 		if type == SHORT_STR:
 			return msg
 		elif type == LONG_STR:
-			return  self.recv().decode()	
+			return  self._recvData().decode()	
 		elif type == INT:
 			return msg
 		elif type == ENUM:
 			return LHMT_Commands(msg)
 		elif type == NUMPY_ARRAY:
-			return self.RecvNumpyArray(msg)
+			return self._recvNumpyArray(msg)
 		elif type == LIST:
 			return msg
 		elif  type == FILE:
-			return self.RecvFile(msg, *args, **kwargs)
+			return self.__recvFile(msg, *args, **kwargs)
 		elif type == PICKLE:
-			bytes = self.recv()
+			bytes = self._recvData()
 			ret = pickle.loads(bytes)
 			return ret
 			
@@ -269,92 +266,16 @@ class Socket():
 	# @see Send
 	# @see Recv
 	def SendRecv(self, msg):
-		self.Send(msg)
-		return self.Recv()
+		self._send(msg)
+		return self._recvData()
 	
 	## Recieve a msg and immediatly respond
 	# @see Send
 	# @see Recv
 	def RecvSend(self, msg):
-		ret = self.Recv()
-		self.Send(msg)
+		ret = self._recvData()
+		self._send(msg)
 		return ret
-
-
-## Construct and connect a SocketHandler
-def Connect(ip, port, callback = lambda: None):
-	sock = SocketHandler(socket.socket(socket.AF_INET, socket.SOCK_STREAM),callback)
-	sock.settimeout(2)
-	sock.connect((ip, port))
-	sock.settimeout(None)
-	return sock
-		
-from threading import Lock
-from threading import Thread
-
-## This class fetches images from the server.
-#
-# This is done on a seperate thread.
-class SocketVideoStream:
-	def __init__(self,streamSocket):	
-		self.socket = streamSocket
-		
-		self.lock = Lock()
-		self.image = np.zeros(16).reshape(4,4).astype(np.uint8)
-		self.running = False
-		self.width = 0
-		self.height = 0
-
-	def start(self):
-		if not self.running:
-			self.running = True
-			def Update():		
-				try:
-					while self.running:
-						self.image = self.socket.SendRecv(LHMT_Commands.CAMERA_SEND_IMAGE)
-				except LHMT_ConnectionLost as e:
-					PrintException()
-					self.stopped = True
-					self.running = False
-					with Status("Cleaning up"): e.socket.Delete()
-					
-				except:
-					PrintException()
-					self.stopped = True
-					self.running = False
-				self.stopped = True
-			self.t = Thread(target=Update, args=())
-			self.t.daemon = True
-			self.t.start()
-			
-		return self	
-
-	def read(self):
-		return self.image.copy()
-	def GetSize(self):
-		return (self.width, self.height)
-		
-	def stop(self):
-		if self.running:
-			self.stopped = False
-			self.running = False
-			while not self.stopped:
-				time.sleep(0.1)
-	def Resize(self, width, height):
-		if not (self.width == width and self.height == height):
-			with Status("Telling server to resizing camera"):
-				self.running = False
-				self.t.join()
-				self.width = width
-				self.height = height
-				self.socket.Send(LHMT_Commands.SET_RESOLUTION)
-				self.socket.Send((width, height))
-				self.start()
-	def SetExposureTime(self, exptime):
-		pass
-		
-	def SyncThread(*a):
-		pass	
 
 if __name__ == "__main__":
 	class Data():
@@ -436,7 +357,7 @@ if __name__ == "__main__":
 				self.client.Send(msg)
 				s = self.connection.Recv()
 				assert s == msg
-		def SendNumpyArray(self):
+		def _sendNumpyArray(self):
 			with Status_Info("Small int"):
 				array = np.array([[1,2,3],[4,5,6]])
 				self.connection.Send(array)
@@ -468,7 +389,7 @@ if __name__ == "__main__":
 			assert recv.a == data.a
 			assert recv.b == data.b
 			assert recv.c == data.c
-		def SendFile(self):
+		def _sendFile(self):
 			with open("Test.txt", "w") as f:
 				f.write("Test")
 			with open("Test.txt", "rb") as f:
@@ -489,17 +410,17 @@ if __name__ == "__main__":
 	with Status_Info("SendInt"): test.SendInt()
 	with Status_Info("SendShortStr"): test.SendShortString()
 	with Status_Info("SendLongStr"): test.SendLongString()
-	with Status_Info("SendNumpyArray"): test.SendNumpyArray()
+	with Status_Info("_sendNumpyArray"): test._sendNumpyArray()
 	with Status_Info("SendList"): test.SendList()
 	with Status_Info("SendClass"): test.SendClass()
-	with Status_Info("SendFile"): test.SendFile()
+	with Status_Info("_sendFile"): test._sendFile()
 	with Status_Info("SendEnum"): test.SendEnum()
 	'''with Status_Info("Bluetooth socket"):
 		with Status_Info("Initiating test"): test = Test(bluetooth = True)
 		with Status_Info("SendInt"): test.SendInt()
 		with Status_Info("SendShortStr"): test.SendShortString()
 		with Status_Info("SendLongStr"): test.SendLongString()
-		with Status_Info("SendNumpyArray"): test.SendNumpyArray()
+		with Status_Info("_sendNumpyArray"): test._sendNumpyArray()
 		with Status_Info("SendList"): test.SendList()
 		with Status_Info("SendClass"): test.SendClass()
-		with Status_Info("SendFile"): test.SendFile()'''
+		with Status_Info("_sendFile"): test._sendFile()'''
